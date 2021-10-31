26e79a6b861ca84fecae4ce9fdc999939d1cef5f
Rishav Nandi <rishavnandi06@gmail.com>
2021-10-31 18:48:55 +0530

    added search bar and features
    Signed-off-by: Rishav Nandi <rishavnandi06@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import Movie from "./components/Movie";
 
 const FEATURED_API =
@@ -7,19 +7,52 @@ const SEARCH_API =
   "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 function App() {
-  const [movies,setMovies] = useState([]);
-  useEffect(()=>{
-      fetch(FEATURED_API).then((res)=>res.json())
-      .then((data)=>{
-        setMovies(data.results)
-      })
-  },[])
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  return <div className="container">
-    {movies.length > 0 && movies.map((movie)=>(
-      <Movie {...movie} key={movie.id} />
-    ))}
-  </div>
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm);
+      setSearchTerm("");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  return (
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="search"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </header>
+      <div className="container">
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie {...movie} key={movie.id} />)}
+      </div>
+    </>
+  );
 }
 
 export default App;
